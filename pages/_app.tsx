757e463edbaa9e2d3a75765de6623783d987bf58
notstoe <gstonin95@gmail.com>
2021-11-23 18:36:18 +0000

    apollo configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,42 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+import { NextComponentType, NextPageContext } from "next";
+
+import { ApolloProvider } from "@apollo/client";
+import withData from "../lib/withData";
+
+type AppProps = {
+	pageProps: any;
+	Component: NextComponentType<NextPageContext, any, {}>;
+	apollo: any;
+};
+
+type InitialProps = {
+	Component: NextComponentType<NextPageContext, any, {}>;
+	ctx: any;
+	pageProps: any;
+};
+
+function MyApp({ Component, pageProps, apollo }: AppProps) {
+	return (
+		<ApolloProvider client={apollo}>
+			<Component {...pageProps} />
+		</ApolloProvider>
+	);
 }
 
-export default MyApp;
+// boilerplate code, to make sure that every page that has getInitialProps, it will wait for it to be fetched
+// every page will have getInitialProps cause withData() is adding it to them
+
+MyApp.getInitialProps = async function ({ Component, ctx }: InitialProps) {
+	let pageProps = {};
+	if (Component.getInitialProps) {
+		pageProps = await Component.getInitialProps(ctx);
+	}
+	pageProps.query = ctx.query;
+	return { pageProps };
+};
+
+// exporting the app through withData function, this way the apollo prop sends the data through MyApp to the <ApolloProvider>
+
+export default withData(MyApp);
