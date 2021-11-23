6402e9866796ebbc0984c3a7c3c130eee72c714b
notstoe <gstonin95@gmail.com>
2021-11-23 18:36:40 +0000

    apollo fetch configs testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,41 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { gql, useQuery } from "@apollo/client";
 
 const Home: NextPage = () => {
+	const ALL_PAINTINGS_QUERY = gql`
+		query ALL_PAINTINGS {
+			paintings {
+				Title
+				Price
+				Picture {
+					url
+				}
+			}
+		}
+	`;
+
+	const { data, loading, error } = useQuery(ALL_PAINTINGS_QUERY);
+
+	if (loading) return "Loading...";
+	if (error) return JSON.stringify(error);
+
+	// console.log({ data });
+
+	// const paintings = data.paintings.map((painting, index) => (
+	// 	<div key={index}>
+	// 		{painting.Title + " " + painting.Price}
+	// 		<img src={painting.Picture[0].url} style={{ width: "200px" }}></img>
+	// 		<img src={painting.Picture[1].url} style={{ width: "200px" }}></img>
+	// 	</div>
+	// ));
+
 	return (
 		<div>
 			<Head>{/* <link rel="icon" href="/favicon.ico" /> */}</Head>
 
-			<main>Hello World</main>
+			<main></main>
+
 			<footer></footer>
 		</div>
 	);
