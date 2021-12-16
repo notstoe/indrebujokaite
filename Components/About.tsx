3a93319fbb9e1df88843553a2ff86d278ab04863
notstoe <gstonin95@gmail.com>
2021-12-16 16:47:43 +0000

    types added, moved title to a component, sc syntax

--- Components/About.tsx
+++ Components/About.tsx
@@ -1,8 +1,10 @@
-import { gql, useQuery } from "@apollo/client";
+import { ApolloError, gql, useQuery } from "@apollo/client";
 
 import styled from "styled-components";
 
-const StyledAboutSectionWrapper = styled.section`
+import SectionTitleBig from "./SectionTitleBig";
+
+const SectionWrapper = styled.section`
 	width: 95%;
 	max-width: 1500px;
 	margin: 0 auto;
@@ -11,53 +13,8 @@ const StyledAboutSectionWrapper = styled.section`
 	padding: 0;
 
 	font-weight: lighter;
-
-	display: flex;
-	flex-direction: column;
-
-	.title {
-		font-size: 10rem;
-
-		@media (max-width: 615px) {
-			font-size: 6.5rem;
-		}
-
-		@media (max-width: 450px) {
-			font-size: 4.5rem;
-		}
-	}
-
-	.first {
-		position: relative;
-
-		::after {
-			content: "";
-			display: block;
-			position: absolute;
-			top: -13vh;
-			right: 29vw;
-
-			z-index: -2;
-
-			width: 23rem;
-			height: 23rem;
-			border-radius: 50%;
-
-			background-color: var(--dark-grey);
-		}
-	}
-
-	.left {
-		line-height: 10rem;
-		text-align: left;
-	}
-	.right {
-		line-height: 10rem;
-		text-align: right;
-	}
 `;
-
-const StyledInnerContentWrapper = styled.div`
+const InnerContentDiv = styled.div`
 	display: flex;
 	flex-direction: column;
 	line-height: 1.9rem;
@@ -89,11 +46,20 @@ const StyledImage = styled.img`
 	padding-left: 1rem;
 `;
 
-interface DataA {
-	about: {
-		__typename: string;
-		about_me_txt: string;
+const s = { StyledImage, InnerContentDiv, SectionWrapper };
+
+interface aboutMeInfo {
+	about_me_txt: string;
+	ProfilePic: {
+		id: string;
+		url: string;
 	};
+}
+
+export interface DataA {
+	about: aboutMeInfo;
+	loading: boolean;
+	error?: ApolloError;
 }
 
 export default function About() {
@@ -101,33 +67,33 @@ export default function About() {
 		query ABOUT_ME_TXT {
 			about {
 				about_me_txt
+				ProfilePic {
+					url
+				}
 			}
 		}
 	`;
 
 	const { data, loading, error } = useQuery<DataA>(ABOUT_ME_QUERY);
 
-	if (loading) return <div>Loading...</div>;
-	if (error) return <div>{JSON.stringify(error)}</div>;
+	if (loading) return <div style={{ textAlign: "center" }}>Loading...</div>;
+	if (error)
+		return (
+			<div style={{ textAlign: "center" }}>
+				Something went wrong... Try refreshing the page
+			</div>
+		);
 
 	return (
-		<StyledAboutSectionWrapper>
-			<div className="left first title">
-				<span>ABOUT ME</span>
-			</div>
-			<div className="right title">
-				<span>ABOUT ME</span>
-			</div>
-			<div className="left title">
-				<span>ABOUT ME</span>
-			</div>
-			<StyledInnerContentWrapper>
+		<s.SectionWrapper>
+			<SectionTitleBig title="ABOUT ME" topBottomPos="left" middlePos="right" />
+			<s.InnerContentDiv>
 				<span>{data?.about.about_me_txt}</span>
-				<StyledImage
-					src="https://res.cloudinary.com/dowa8tjdi/image/upload/v1639500322/profile_2_nslztr.jpg"
-					alt="Indre's picture"
+				<s.StyledImage
+					src={data?.about.ProfilePic.url}
+					alt="Artist's Profile"
 				/>
-			</StyledInnerContentWrapper>
-		</StyledAboutSectionWrapper>
+			</s.InnerContentDiv>
+		</s.SectionWrapper>
 	);
 }
