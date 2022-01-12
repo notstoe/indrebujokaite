21cb48b923ecdc62c57602714666c59724fd70d7
notstoe <gstonin95@gmail.com>
2022-01-12 19:31:56 +0000

    tidying up apollo config file

--- lib/withData.ts
+++ lib/withData.ts
@@ -1,11 +1,14 @@
 // configs for the apollo client
 
-import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
-import { getDataFromTree } from "@apollo/client/react/ssr";
-import { createUploadLink } from "apollo-upload-client";
-import withApollo from "next-with-apollo";
-import { endpoint, prodEndpoint } from "../config";
+import {
+	ApolloClient,
+	ApolloLink,
+	createHttpLink,
+	InMemoryCache,
+} from '@apollo/client';
+import { getDataFromTree } from '@apollo/client/react/ssr';
+import withApollo from 'next-with-apollo';
+import { endpoint, prodEndpoint } from '../config';
 
 type withDataProps = {
 	initialState?: any;
@@ -15,22 +18,10 @@ type withDataProps = {
 function withData({ headers, initialState }: withDataProps): ApolloClient<any> {
 	return new ApolloClient({
 		link: ApolloLink.from([
-			onError(({ graphQLErrors, networkError }) => {
-				if (graphQLErrors)
-					graphQLErrors.forEach(({ message, locations, path }) =>
-						console.log(
-							`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-						)
-					);
-				if (networkError)
-					console.log(
-						`[Network error]: ${networkError}. Backend is unreachable. Is it running?`
-					);
-			}),
-			createUploadLink({
-				uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
+			createHttpLink({
+				uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
 				fetchOptions: {
-					credentials: "include",
+					credentials: 'include',
 				},
 				headers,
 			}),
