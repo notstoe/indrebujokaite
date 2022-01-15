da7bad98ea504b13a7a2281e81dfe9e2155f0ee7
notstoe <gstonin95@gmail.com>
2022-01-15 18:16:50 +0000

    tidying up types

--- Components/Contact/Contact.types.ts
+++ Components/Contact/Contact.types.ts
@@ -1,6 +1,6 @@
 import { ApolloError } from '@apollo/client';
 
-interface ContactInfo {
+type ContactInfo = {
 	name: string;
 	email: string;
 	phone: string;
@@ -8,18 +8,10 @@ interface ContactInfo {
 	shipping_info: string;
 	instagram: string;
 	facebook: string;
-}
+};
 
 export interface DataContact {
-	contact: {
-		name: string;
-		email: string;
-		phone: string;
-		location_based: string;
-		shipping_info: string;
-		instagram: string;
-		facebook: string;
-	};
+	contact: ContactInfo;
 	loading: boolean;
 	error?: ApolloError;
 }
