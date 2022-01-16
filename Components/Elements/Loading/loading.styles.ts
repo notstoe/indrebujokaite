48d2f937166425150c120e9bb1f182c37705eeff
notstoe <gstonin95@gmail.com>
2022-01-16 18:39:08 +0000

    sligh styles adjust

--- Components/Elements/Loading/loading.styles.ts
+++ Components/Elements/Loading/loading.styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 const Loading = styled.div`
+	width: 100%;
 	text-align: 'center';
-	font-size: '2rem';
-	padding: '5rem';
+	font-size: 1.8rem;
+	padding: 5rem;
 `;
 
 export const ss = { Loading };
