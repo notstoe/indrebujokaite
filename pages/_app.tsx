bd7b80c314905e96c34cde073ddf39d84e77dab6
notstoe <gstonin95@gmail.com>
2022-01-27 16:04:00 +0000

    Add visual cue to route changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import Page from 'Components/Page/Page';
 
 import { AppProps } from 'next/app';
+import { Router } from 'next/router';
+
+import NProgress from 'nprogress';
+import 'styles/nprogress.css';
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
